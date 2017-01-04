d39f727fbb158b2ce184637b46f820ebdb2efb4b
Rodolfo Lam <r_lam@hotmail.com>
2017-01-04 00:55:45 -0500

    Preliminary test of #1

--- UDPReader/src/UDPReader.cpp
+++ UDPReader/src/UDPReader.cpp
@@ -25,11 +25,32 @@ int main(int argc, char** argv) {
 
     std::unordered_map<RL::Network::DataTypesEnum, std::array<float, 8>,
             RL::Network::DataTypesEnumHash> parsedData;
+    parsedData.reserve(46);
+    for (auto const &ref : RL::Network::Data_Index) {
+        std::array<float, 8> ar;
+        parsedData.emplace(std::make_pair(ref.second, std::move(ar)));
+    }
+
+    RL::Network::MessageParser parser;
 
     for (;;) {
         Poco::Net::SocketAddress sender;
         int n = dgs.receiveFrom(buffer.data(), RL::Network::NETMESSAGE_SIZE,
                 sender);
+
+        parser.ParsePacket(parsedData, buffer, n);
+        if (parser.LastPacketParseCorrect()) {
+            for (const auto &record : parsedData) {
+                for (const auto &rows : record.second) {
+                    std::cout << rows << ", ";
+                }
+
+                std::cout << std::endl;
+            }
+        } else {
+            std::cerr << "ERROR: " << parser.LastPacketParseStatusCode()
+                    << std::endl;
+        }
     }
 
     return 0;
