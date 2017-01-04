b5873a79e863ddf02f7d1873c3a2a0b2adfe7147
Rodolfo Lam <r_lam@hotmail.com>
2017-01-03 22:18:03 -0500

    Clean main function
    Will begin testing of MessageParser class to validate #1

--- UDPReader/src/UDPReader.cpp
+++ UDPReader/src/UDPReader.cpp
@@ -5,64 +5,31 @@
 // Copyright   : (C) 2016 Rodolfo Lam. All Rights Reserved.
 // Description : Get one DATA-type package and display its contents.
 //============================================================================
-#include "Poco/Net/DatagramSocket.h"
-#include "Poco/Net/SocketAddress.h"
-#include "Poco/ByteOrder.h"
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <algorithm>
 #include <array>
+#include <unordered_map>
+#include <Poco/Net/DatagramSocket.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/ByteOrder.h>
 
 #include "Network/MessageParser.h"
 
 using Poco::ByteOrder;
 
-/* 4793 bytes is maximum with all 133 DATA fields active + 5 bytes of header.
- Rounded for additional buffer space in case its necessary. */
-constexpr int NETMESSAGE_SIZE = 4800;
-
-struct alignas(4) xMessage_struct {
-    Poco::Int8 header[5];
-    Poco::Int32 myIndex;
-    float data[8];
-};
-
 int main(int argc, char** argv) {
-    xMessage_struct myData;
-
-    for (int i = 0; i < 8; i++) {
-        myData.data[i] = 0.0f;
-    }
-
     Poco::Net::SocketAddress sa(Poco::Net::IPAddress(), 49003);
     Poco::Net::DatagramSocket dgs(sa);
 
-    std::array<Poco::UInt8, NETMESSAGE_SIZE> buffer;
+    std::array<Poco::UInt8, RL::Network::NETMESSAGE_SIZE> buffer;
     buffer.fill(0);
+
+    std::unordered_map<RL::Network::DataTypesEnum, std::array<float, 8>,
+            RL::Network::DataTypesEnumHash> parsedData;
 
     for (;;) {
         Poco::Net::SocketAddress sender;
-        int n = dgs.receiveFrom(buffer.data(), NETMESSAGE_SIZE, sender);
-
-        std::memcpy(&myData, buffer.data(), 4); // THE HEADER "DATA" message only
-        myData.header[4] = '\0';
-
-        myData.myIndex = RL::Network::parseIntfromBuffer(&buffer[5]);
-
-        for (int i = 0; i < 8; ++i) {
-            size_t offset = (i * 4) + 9; // i * 4(alignment) + 9(offset of first data[] member
-            myData.data[i] = RL::Network::parseFloatfromBuffer(&buffer[offset]);
-        }
-
-        std::cout << "Bytes received: " << n << " - " << myData.header
-                << " Index: " << myData.myIndex << std::endl;
-
-        for (int i = 0; i < 8; i++) {
-            std::cout << myData.data[i] << " ";
-        }
-
-        std::cout << std::endl;
+        int n = dgs.receiveFrom(buffer.data(), RL::Network::NETMESSAGE_SIZE,
+                sender);
     }
 
     return 0;
