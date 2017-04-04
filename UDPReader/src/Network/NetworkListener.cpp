8cdc2f61c93804a7cfe4b4899fac235869a36044
Rodolfo Lam <r_lam@hotmail.com>
2017-04-03 20:10:18 -0500

    Partial Implementation of NetworkListener class

---
+++ UDPReader/src/Network/NetworkListener.cpp
@@ -0,0 +1,47 @@
+/*
+ * NetworkListener.cpp
+ *
+ *  Created on: Apr 3, 2017
+ *  Copyright (C) 2017 rlam1. See LICENSE file for more information
+ *
+ *  Description: 
+ */
+
+#include <Network/NetworkListener.h>
+
+namespace RL {
+namespace Network {
+
+NetworkListener::NetworkListener() {
+    receiveAddress = Poco::Net::SocketAddress(Poco::Net::IPAddress(), 49003);
+    dataSocket.setBlocking(false);
+    dataSocket.bind(receiveAddress, false);
+
+    buffer.fill(0);
+
+    sizeOfLastPacket = 0;
+}
+
+NetworkListener::~NetworkListener() {
+    dataSocket.close();
+}
+
+Poco::Net::SocketAddress NetworkListener::checkforPacket() {
+    Poco::Net::SocketAddress sender;
+
+    try {
+        sizeOfLastPacket = dataSocket.receiveFrom(buffer.data(),
+                    RL::Network::NETMESSAGE_SIZE, sender);
+    } catch (Poco::TimeoutException &e) {
+        sizeOfLastPacket = 0;
+    }
+
+    return sender;
+}
+
+int NetworkListener::getLastPacketSize() const {
+    return sizeOfLastPacket;
+}
+
+} /* namespace Network */
+} /* namespace RL */
---
+++ UDPReader/src/Network/NetworkListener.h
@@ -0,0 +1,54 @@
+/*
+ * NetworkListener.h
+ *
+ *  Created on: Apr 3, 2017
+ *  Copyright (C) 2017 rlam1. See LICENSE file for more information
+ *
+ *  Description: Handles incoming network traffic
+ */
+
+#ifndef NETWORK_NETWORKLISTENER_H_
+#define NETWORK_NETWORKLISTENER_H_
+
+#include "Network/MessageParser.h"
+
+#include <array>
+#include <unordered_map>
+
+#include <Poco/Net/DatagramSocket.h>
+#include <Poco/Net/SocketAddress.h>
+
+namespace RL {
+namespace Network {
+
+/* NetworkListener
+ * ----------------------------------------------
+ * Wraps the network code into a single location.
+ * Class is responsible for handling the connection setup
+ * and parsing of incoming packets. */
+class NetworkListener {
+public:
+    NetworkListener();
+    virtual ~NetworkListener();
+
+    /* Listens on selected UDP port for new packet.
+     * If found, store the data into the buffer.
+     * Return the address of the sender. */
+    Poco::Net::SocketAddress checkforPacket();
+
+    int getLastPacketSize() const;
+
+private:
+    Poco::Net::SocketAddress receiveAddress;
+    Poco::Net::DatagramSocket dataSocket;
+
+    std::array<Poco::UInt8, RL::Network::NETMESSAGE_SIZE> buffer;
+
+    RL::Network::MessageParser parser;
+    int sizeOfLastPacket;
+};
+
+} /* namespace Network */
+} /* namespace RL */
+
+#endif /* NETWORK_NETWORKLISTENER_H_ */
