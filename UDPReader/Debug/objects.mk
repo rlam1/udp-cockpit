cf455223de9052aa982083bd6e1a367f9e6e4205
Rodolfo Lam <r_lam@hotmail.com>
2018-05-14 11:36:17 -0500

    Delete old build system generated files
    No longer using Eclipse IDE, will default to Cmake now as a build system

--- UDPReader/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lallegro -lallegro_color -lallegro_font -lallegro_image -lallegro_main -lallegro_primitives -lallegro_ttf -lPocoFoundation -lPocoNet -lPocoUtil
-
